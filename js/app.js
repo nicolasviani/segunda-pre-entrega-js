662aef39bf9f32f12e5a87a2dd5a7cf8a07a88d4
nicolas viani <viani.nicolas.87@gmail.com>
2023-05-03 17:23:58 -0300

    inicio de proyecto

---
+++ js/app.js
@@ -0,0 +1,461 @@
+const products = [
+    {
+        productName: "Remeras hombres",
+        options: [
+            {
+                productName: "Remera Jordan",
+                price: "18999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera smile",
+                price: "17999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera seleccion",
+                price: "18999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "camiseta argentina",
+                price: "45999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "musculosa argentina",
+                price: "16999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campeon argentina",
+                price: "20999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera adidas",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera nike jump",
+                price: "27999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera/short nike",
+                price: "32999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "Remeras mujeres",
+        options: [
+            {
+                productName: "musculosas top",
+                price: "9999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera adidas",
+                price: "11999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remeron adidas",
+                price: "14999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remeron nike",
+                price: "14999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "top m/larga nike ",
+                price: "13999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera adidas",
+                price: "14999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera reebok",
+                price: "17999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remeron slide",
+                price: "12999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "remera misbhv",
+                price: "17999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "buzos hombres",
+        options: [
+            {
+                productName: "buzo NR",
+                price: "24999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo nike air",
+                price: "32999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo smile",
+                price: "29999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo nike",
+                price: "23999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campera stone",
+                price: "24999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "chalecos deportivos",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "chaqueta stone",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo adidas",
+                price: "27999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo adidas essen",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "buzos mujeres",
+        options: [
+            {
+                productName: "campera reebok",
+                price: "23999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo capucha nike",
+                price: "28999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo adidas",
+                price: "28999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campera adidas rosa",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campera adidas clarita",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campera adidas celeste",
+                price: "26999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "campera polar nike",
+                price: "28999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo cuello reebok",
+                price: "27999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "buzo hugo boss",
+                price: "28999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "pantalones hombres",
+        options: [
+            {
+                productName: "pantalon adidas",
+                price: "24999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "joggins urbano",
+                price: "23999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. depor. Puma",
+                price: "45999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. depor. nike",
+                price: "52999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. 5piezas fit",
+                price: "56999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "short de baño",
+                price: "13999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "joggins camuflaje",
+                price: "22999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "joggins adidas",
+                price: "27999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "nike calza deportiva",
+                price: "15999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "pantalones mujeres",
+        options: [
+            {
+                productName: "calza corta adidas",
+                price: "16999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "calza nike ",
+                price: "27999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "calza reebok",
+                price: "25999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. calza/chaleco",
+                price: "46999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. calza/top",
+                price: "35999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "conj. nike",
+                price: "25999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "pollera short",
+                price: "13999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "short adidas",
+                price: "13999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+            {
+                productName: "calza biker nike",
+                price: "15999",
+                sizes: ["S", "M", "L", "XL", "XXL"],
+            },
+        ] 
+    },
+    {
+        productName: "zapatillas hombre",
+        options: [
+            {
+                productName: "zapatillas nike air",
+                price: "34999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatilla nike naranja",
+                price: "32999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike air blan",
+                price: "43999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike paddel",
+                price: "46999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike air rojas",
+                price: "43999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike basquet",
+                price: "42999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike air azul",
+                price: "42999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas run adidas",
+                price: "44999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+        ]
+    },
+    {
+        productName: "zapatillas mujeres",
+        options: [
+            {
+                productName: "zapatillas all star",
+                price: "32999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas nike air",
+                price: "35999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas puma",
+                price: "38999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas run adidas",
+                price: "26999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas EQ21 adidas",
+                price: "28999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas adidas trail",
+                price: "32999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas reebok",
+                price: "36999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas classic reebok",
+                price: "32999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+            {
+                productName: "zapatillas legacy",
+                price: "34999",
+                sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+            },
+        ] 
+    },
+];
+
+let userName;
+let cloth;
+let model;
+let price;
+let size;
+let quantity;
+let addProduct;
+let respuesta = "si";
+
+
+alert("Bienvenidos al carrito de compras de Mamba Design...");
+userName = prompt("Ingrese su nombre de usuario...");
+
+const productsMenu = products.map((product,index) => {
+    return "\n" + index + " " + product.productName    
+})
+cloth = parseInt(prompt("¿Que producto deseas comprar?" + productsMenu));
+
+const product = products[cloth]
+const modelsMenu = product.options.map((model,index) => {
+    return "\n" + index + " " + model.productName + " $" + model.price
+})
+model = parseInt(prompt("¿Que modelo deseas llevar?" + modelsMenu));
+
+const sizesMenu = product.options[model].sizes.map((size,index) => {
+    return "\n" + index + " " + size
+})
+size = parseInt(prompt("¿Que talle deseas llevar?" + sizesMenu));
+
+do{
+    quantity = Number(prompt("¿Que cantidad deseas llevar?"));
+}while(quantity <=0);
+
+///// quiero preguntarle al cliente si desea agregar otro producto///////
+do{
+    prompt("¿deseas agregar otro producto? (si/no)");
+}while (respuesta === "no");
+
+function addProduct (){
+    const productsMenu = products.map();
+    cloth = prompt("¿Ingresa el producto que deseas llevar? ${cloth}"); 
+}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
